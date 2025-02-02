910c36200f40e1f5bf32898a166de5ba736d2d0e
regis066 <mugisharegis72@gmail.com>
2025-02-02 21:27:28 +0200

    delete file

--- decode01.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image as PIL
-from pdf417decoder import PDF417Decoder
-
-image = PIL.open("image01.png")
-decoder = PDF417Decoder(image)
-
-if (decoder.decode() > 0):
-    decoded = decoder.barcode_data_index_to_string(0)
